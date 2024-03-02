25498707413c13ce4db4df060e7131c7e0869dfc
alexlampe14 <lampealex888@gmail.com>
2024-03-02 15:20:42 -0500

    add auth0

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="">
-      <a className="btn btn-primary" href="/api/auth/login">
+      <a className="" href="/api/auth/login">
         Login
       </a>
-      <a className="btn btn-secondary" href="/api/auth/logout">
+      <a className="" href="/api/auth/logout">
         Logout
       </a>
-      <Link className="btn btn-accent" href="/profile">Profile</Link>
+      <Link className="" href="/profile">Profile</Link>
     </main>
   );
 }
