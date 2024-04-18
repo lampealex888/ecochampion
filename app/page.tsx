8c6a02025943da7396e7463b0e42178cec7ebb59
lampealex888 <lampealex888@gmail.com>
2024-04-18 18:01:54 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+        {/* <section className="w-full py-12 md:py-24 lg:py-32 border-t">
           <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
             <div className="space-y-3">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
@@ -94,7 +94,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
         <section className="bg-gray-50 dark:bg-gray-950 py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
