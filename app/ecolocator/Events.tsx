9b127e79a2bf4dc5755beee94deb62609c509915
Imran Haidery <iahaidery@gmail.com>
2024-04-17 23:37:29 -0400

    Update Events.tsx

--- app/ecolocator/Events.tsx
+++ app/ecolocator/Events.tsx
@@ -36,15 +36,19 @@ export default function Events() {
 
   return (
     <div>
-      <h2 className="text-lg font-semibold mb-2">Events Nearby:</h2>
+      <h2 className="text-lg font-semibold mb-2 dark:text-gray-200">Events Nearby:</h2>
       <ul>
         {events.map((event, index) => (
           <li key={index}>
-            <h2> <a href={event.link} style={{ color: 'blue', textDecoration: 'underline' }}> {event.title} </a></h2>
-            <p>Start Date: {event.date.start_date}</p>
-            <p>When: {event.date.when}</p>
-            <p>Address: {event.address.join(', ')}</p>
-            <img src={event.thumbnail} alt={event.title} />
+            <div className="bg-blue-100 dark:bg-gray-800 p-4 my-2 rounded flex items-start">
+              <div>
+                <h2> <a href={event.link} className="text-blue-500 underline dark:text-blue-400"> {event.title} </a></h2>
+                <p className="dark:text-gray-200">Start Date: {event.date.start_date}</p>
+                <p className="dark:text-gray-200">When: {event.date.when}</p>
+                <p className="dark:text-gray-200">Address: {event.address.join(', ')}</p>
+              </div>
+              <img className="ml-4" src={event.thumbnail} alt={event.title} />
+            </div>
           </li>
         ))}
       </ul>
